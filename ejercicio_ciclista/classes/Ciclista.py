fddc3b2a2b839684faeef0e0bb766e51d66d7d0c
jhonatan.ossa <jhonatan.ossa@ceiba.com.co>
2022-05-26 00:10:58 -0500

    se crea la carpeta clasess y s ecrea la clase ciclista

---
+++ ejercicio_ciclista/classes/Ciclista.py
@@ -0,0 +1,46 @@
+class Ciclista:
+    def __init__(self):
+        self.__id = None
+        self.__nombre = None
+        self.__edad = None
+        self.__pais = None
+        self.__equipo = None
+        self.__tiempo = None
+
+    @property
+    def id(self):
+        return (self.__id)
+    @property
+    def nombre(self):
+        return (self.__nombre)
+    @property
+    def edad(self):
+        return (self.__edad)
+    @property
+    def pais(self):
+        return (self.__pais)
+    @property
+    def equipo(self):
+        return (self.__equipo)
+    @property
+    def tiempo(self):
+        return (self.__tiempo)
+
+    @id.setter
+    def id(self,id):
+        self.__id=id
+    @nombre.setter
+    def nombre(self,nombre):
+        self.__nombre=nombre
+    @edad.setter
+    def edad(self,edad):
+        self.__edad=edad
+    @pais.setter
+    def pais(self,pais):
+        self.__pais=pais
+    @equipo.setter
+    def equipo(self,equipo):
+        self.__equipo=equipo
+    @tiempo.setter
+    def tiempo(self,tiempo):
+        self.__tiempo=tiempo
